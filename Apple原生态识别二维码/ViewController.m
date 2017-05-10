e702900663f7d9cb86d1856ee6aa76a3943b2080
fengxueSY <xyz123003@126.com>
2017-05-10 09:02:48 +0800

    add

--- Apple原生态识别二维码/ViewController.m
+++ Apple原生态识别二维码/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController ()<AVCaptureMetadataOutputObjectsDelegate>{
+//    AVCaptureSession * _capture;
+    AVCaptureSession * session;//输入输出的中间桥梁
+}
 
 @end
 
@@ -17,8 +21,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //获取摄像设备
+    AVCaptureDevice * device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    //创建输入流
+    AVCaptureDeviceInput * input = [AVCaptureDeviceInput deviceInputWithDevice:device error:nil];
+    //创建输出流
+    AVCaptureMetadataOutput * output = [[AVCaptureMetadataOutput alloc]init];
+//    output.rectOfInterest=CGRectMake(0.5,0,0.5, 1);
+    //设置代理 在主线程里刷新
+    [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    
+    //初始化链接对象
+    session = [[AVCaptureSession alloc]init];
+    //高质量采集率
+    [session setSessionPreset:AVCaptureSessionPresetHigh];
+    
+    [session addInput:input];
+    [session addOutput:output];
+    //设置扫码支持的编码格式(如下设置条形码和二维码兼容)
+    output.metadataObjectTypes=@[AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code];
+    
+    AVCaptureVideoPreviewLayer * layer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    layer.videoGravity=AVLayerVideoGravityResizeAspectFill;
+    layer.frame = CGRectMake((self.view.frame.size.width - 300) / 2, 20, 300, 300);
+    [self.view.layer insertSublayer:layer atIndex:0];
+    //开始捕获
+    [session startRunning];
 }
-
+-(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
+    if (metadataObjects.count>0) {
+        [session stopRunning];
+        AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex : 0 ];
+        //输出扫描字符串
+        NSLog(@"%@",metadataObject.stringValue);
+        NSString * str = [NSString stringWithFormat:@"%@",metadataObject.stringValue];
+       [self mate:str];
+    }
+}
+-(void)mate:(NSString *)str{
+    UILabel * label = [[UILabel alloc]initWithFrame:CGRectMake(10, 350, self.view.frame.size.width - 20, 300)];
+    label.numberOfLines = 0;
+    label.text = str;
+    [self.view addSubview:label];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
